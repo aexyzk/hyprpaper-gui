6a42053f742b4b04f27a52c3f6cd52107c4fbaf1
aexyzk <aexyzk@proton.me>
2024-06-21 08:00:36 -0500

    what am i doing with my life

--- main.py
+++ main.py
@@ -15,8 +15,7 @@ def main():
     make_folder(path, preview_path)
     padding = 10
     size = 128
-    size = 250
-    column_count = 5
+    column_count = 8
     
     screen_width = ((column_count * (size + padding)) + padding) + 10
 
@@ -27,7 +26,7 @@ def main():
     preview_images_links, images_links, images = reload_images(path, preview_path, column_count, size, padding)
 
     offset = 0
-    mouse_sens = 30
+    mouse_sens = 40
 
     # ui elements
     bar_rect = pygame.Rect(0, screen.get_height() - 48, screen.get_width() - 10, 48)
@@ -36,12 +35,13 @@ def main():
 
     # assets
     assets = {
-        "refresh_button": load_image("refresh.png")
+        "refresh_button": load_image("refresh.png"),
+        "loading": load_image("loading.png")
     }
 
     running = True
     while running:
-        clamp = (preview_images_links[1] * -(size + padding)) + screen.get_height() - size - padding
+        clamp = (preview_images_links[1] * -(size + padding) - padding) + screen.get_height() - size - 48 - padding
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -49,30 +49,30 @@ def main():
                 if event.button == 1:
                     if bar_rect.collidepoint((pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])):
                         if refresh_button_rect.collidepoint((pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])):
+                            screen.blit(assets["loading"], (0,screen.get_height() - 48))
+                            pygame.display.flip()
+                            clock.tick(60)
                             create_previews(path, preview_path, size)
+                            preview_images_links, images_links, images = reload_images(path, preview_path, column_count, size, padding)
                         break
                     for i, image in enumerate(images):
                         rect = pygame.Rect(
                                 preview_images_links[0][i][1][0] * (size + padding) + padding,
-                                preview_images_links[0][i][1][1] * (size + padding) + padding + offset,
+                                preview_images_links[0][i][1][1] * (size + padding) + padding,
                                 size,size)
                         if rect.collidepoint((pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1] - offset)):
                             set_wallpaper(os.path.join(path, images_links[0][i][0]))
 
                 if event.button == 4:
                     offset = min(offset + mouse_sens, 0)
-                    #offset += mouse_sens
                 elif event.button == 5:
                     offset = max(offset - mouse_sens - 48, clamp)
-                    #offset -= mouse_sens
-
         screen.fill((25,25,25))
         for i, image in enumerate(images):
             rect = pygame.Rect(
                                 preview_images_links[0][i][1][0] * (size + padding) + padding,
                                 preview_images_links[0][i][1][1] * (size + padding) + padding + offset,
                                 size,size)
-
             pygame.draw.rect(screen, (35,35,35), rect)
         render(screen, images, offset, size)
         bar(screen, assets, bar_rect, scroll_bar_rect, refresh_button_rect, offset, clamp)
@@ -88,15 +88,13 @@ def bar(screen, assets, bar_rect, scroll_bar_rect, refresh_button_rect, offset, 
     # bar
     pygame.draw.rect(screen, (40,40,40), bar_rect)
     screen.blit(assets["refresh_button"], refresh_button_rect.topleft)
-
     # scroll bar
     pygame.draw.rect(screen, (45,45,45), scroll_bar_rect)
     # scroll handle
-    handle_size = ((clamp * screen.get_height()) * -1)
-    handle_pos = (-clamp * -offset - handle_size) // screen.get_height()
+    total_height = screen.get_height()
+    handle_size = max(int(total_height * total_height / (-clamp)), 20)
+    handle_pos = int(-offset * (total_height - handle_size) / (-clamp))
     pygame.draw.rect(screen, (80,80,80), pygame.Rect(screen.get_width() - 10, handle_pos, 10, handle_size))
-    print(f"{screen.get_height()} {-offset}")
-    print(handle_pos)
 
 def get_preview_images(preview_images_links, _path, size, padding):
     images = []
@@ -105,12 +103,10 @@ def get_preview_images(preview_images_links, _path, size, padding):
             path = os.path.join(_path, name)
             image = pygame.image.load(path).convert()
             image_size = image.get_size()
-
             centered_pos = (
                 pos[0] * (size + padding) + padding + (size - image_size[0]) // 2,
                 pos[1] * (size + padding) + padding + (size - image_size[1]) // 2
             )
-
             images.append((image, centered_pos))
         except Exception as e:
             print(f"[ERROR] {name}: {e}")
@@ -125,7 +121,6 @@ def update_images(path, offset):
             if i % offset == offset - 1:
                 row_number += 1
             images.append((j,(i % offset, row_number)))
-    print(images)
     return (images, row_number)
 
 def make_folder(path, preview_path):
@@ -136,11 +131,16 @@ def create_previews(path, preview_path, size):
     if not os.path.exists(preview_path):
         os.makedirs(preview_path)
     if os.path.exists(path):
+        # clear dir
+        for i in os.listdir(preview_path):
+            os.remove(os.path.join(preview_path, i))
+            print(f"Removed the preview image for '{i}'")
+
+        # create previews
         row_number = 0
         for i, j in enumerate(sorted(os.listdir(path))):
-            print(j)
+            print(f"Created a preview image for '{j}'")
             create_preview(j, path, preview_path, (size, size))
-
 
 def reload_images(path, preview_path, column_count, size, padding):
     preview_images_links = update_images(preview_path, column_count)
